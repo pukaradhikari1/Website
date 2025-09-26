ab8a3ee8384c379fcd042978b5e51b178ec4db12
pukaradhikari1 <pukar.adhikari656@gmail.com>
2025-09-26 20:25:33 +0545

    projects updated

--- src/components/section/Projects.jsx
+++ src/components/section/Projects.jsx
@@ -29,7 +29,7 @@ export const Projects = () => {
                                     </span>
                                 ))}
                             </div>
-                            <div className="flex justifty-between items-center">
+                            <div className="flex justify-between items-center">
                                 <a href="https://github.com/pukaradhikari1/EndSemProject-1"
                                     className="text-blue-400 hover:text-blue-300 transition-colors my-4">
                                     View Project➡️
@@ -55,7 +55,7 @@ export const Projects = () => {
                                     </span>
                                 ))}
                             </div>
-                            <div className="flex justifty-between items-center">
+                            <div className="flex justify-between items-center">
                                 <a href=""
                                     className="text-blue-400 hover:text-blue-300 transition-colors my-4">
                                     View Project➡️
@@ -63,9 +63,63 @@ export const Projects = () => {
 
                             </div>
                         </div>
+
+
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1
+                     hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                            <h3 className="text-xl font-bold mb-2 ">SolveIt</h3>
+                            <p className="text-gray-400 mb-4">
+                                SolveIt is a collaborative Q&A platform designed for university students to ask, answer, and discuss
+                                course-related questions. It allows students to post questions with tags and file attachments, get answers from peers,
+                                and engage in academic problem-solving. With features like voting, file sharing,
+                                and authentication via OTP, SolveIt creates an interactive and supportive learning community.
+                            </p>
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {["python", "Flask", "React", "sqlite", "tailwind"].map((tech, key) => (
+                                    <span
+                                        key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm
+                                     hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <a href="https://github.com/pukaradhikari1/SolveIt"
+                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4">
+                                    View Project➡️
+                                </a>
+
+                            </div>
+                        </div>
+
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1
+                     hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                            <h3 className="text-xl font-bold mb-2 ">Face Recognizition</h3>
+                            <p className="text-gray-400 mb-4">
+                                A simple python based project that helps detects the face of the person in front of the camera,
+                                or the photo uploaded.
+                            </p>
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {["python", "OpenCV"].map((tech, key) => (
+                                    <span
+                                        key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm
+                                     hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <a href=""
+                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4">
+                                    View Project➡️
+                                </a>
+                            </div>
+                        </div>
                     </div>
-
-
                 </div>
             </RevealOnScroll>
         </section>
